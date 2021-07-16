27ada138dc76d625ffc64e28979c9b8a5ba6d36e
DuhyeongKim <21400080@handong.edu>
2021-07-16 11:15:36 +0900

    no-ply

--- lib/src/utils/turn_web.dart
+++ lib/src/utils/turn_web.dart
@@ -6,7 +6,7 @@ Future<Map> getTurnCredential(String host, int port) async {
   final res = await http.get(Uri.parse(url));
   if (res.statusCode == 200) {
     var data = json.decode(res.body);
-    print('resp => $data.');
+    print('re => $data.');
     return data;
   }
   return {};
